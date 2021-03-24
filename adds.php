74193833df1f132725ab9a04672a8f78a77721ba
Ks-mian <Aqvilarip@gmail.com>
2021-03-24 12:15:08 +0300

    Work 10 2.0

--- adds.php
+++ adds.php
@@ -1,13 +1,15 @@
 <?php
 require_once ('functions.php');
 require 'bd_connect.php';
+
 if ($_SERVER['REQUEST_METHOD']== 'POST') {
 
     $required_fields = ['lot-name', 'category', 'message', 'lot-rate', 'lot-step', 'lot-date'];
     $errors = [];
     foreach ($required_fields as $field) {
         if (empty($_POST[$field])) {
-            $errors[$field] = 'Заполните поле';
+            $errors[$field] = "form__item--invalid";
+            $form_errors='form--invalid';
 
         }
         if ($field == 'lot-rate') {
@@ -27,10 +29,23 @@ if ($_SERVER['REQUEST_METHOD']== 'POST') {
             }
         }
     }
+    if(isset($_FILES['lotPhotos'])){
+        $finfo = finfo_open(FILEINFO_NINE_TYPE);
+        $file_name = $_FILES['lotPhotos']['name'];
+        $file_path = __DIR__ . '/img/';
+        $file_tmpname = $_FILES['lotPhotos']['tmp_name'];
+        $file_type = finfo_file($finfo, $file_tmpname);
+        if($file_type == 'image/gif'){
+            move_uploaded_file($_FILES['lotPhotos']['tmp_name'], $file_path . $file_name);
+        }
+        $file_url='img/' . $file_name;
+
+    }
 
     if (count($errors) !== 0) {
         $page_content = compile_template('add.php',
-            ['errors' => $errors]);
+            ['errors' => $errors,
+            'categories_list' => $categories_list]);
     } else {
         $lot = [
             "image" => file_url ? 'img/user.jpg' : '',
@@ -40,23 +55,34 @@ if ($_SERVER['REQUEST_METHOD']== 'POST') {
             "timer" => $_POST['lot-date'],
             "category" => $_POST['category'],
             "description" => $_POST['message'],
+            "account_id" => $_SESSION['auth']['account_id']
 
         ];
+        $addLotContent = renderTemplate('templates/lotTemplace.php', [
+            'lot' => $lot,
+            'rates' => [],
+            'price' => $lot['start_price']+$lot['rate']
+        ]);
+        $con = mysqli_connect('127.0.0.1', 'root','','yeticave');
+        mysqli_set_charset($con, 'utf8');
+        $sql="SELECT categ_id FROM categories WHERE name='{$lot['category']}'";
+        $result=mysqli_query($con,$sql);
+        $lot['category']=mysqli_fetch_assoc($result)['categ_id'];
+
+        $sql="INSERT INTO lots(lot_name, lot_description, lot_img, lot_categ_id, lot_first_price, lot_end_date, lot_step, lot_categ_id)
+VALUE ('{$lot['lot_name']}', '{$lot['lot_description']}', '{$lot['lot_img']}', '{$lot['lot_categ_id']}', '{$lot['lot_first_price']}', '{$lot['lot_end_date']}', '{$lot['lot_step']}' , '{$lot['lot_categ_id']}')" ;
+        $result=mysqli_query($con,$sql);
+        if(!$result = mysqli_error($con));
     }
 }
+ else{
+     $page_content = compile_template('add.php',
+         ['categories_list' => $categories_list,
+             'data_list' => $data_list
+         ]);
+ }
 
-print_r($lot);
-print_r($errors);
 
-
-
-
-$page_content = compile_template('add.php',
-    ['categories_list' => $categories_list,
-        'lot'=>$currentlot,
-        'lotID'=>$lotID,
-        'data_list' =>$data_list,
-        'lot_time_remaining'=>$lot_time_remaining]);
 
 $layout_content = compile_template('layout.php',
     ['page_layout'=>'Главная страница',
